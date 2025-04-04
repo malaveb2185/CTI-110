aaf0191f4d6c2570ff5f4b48dcec0d1edada1a4d
malaveb2185 <malaveb2185@student.faytechcc.edu>
2025-04-04 12:05:39 -0400

    Add files via upload

---
+++ P1HW1_MalaveBrandon.py
@@ -0,0 +1,26 @@
+# Brandon Malave
+# 4/4/2025
+# CTI-110
+#P1HW1
+
+print("-----Calculating Exponents-----")
+print("\n")
+
+base= int(input("Enter an integer as base value: "))
+exponent=  int(input("Enter an integer as exponent: "))
+
+#Calculate and Display
+print("\n")
+print(base, "raised to the power of", exponent, "is", base**exponent, "!!")
+
+
+print("-----Addition and Subtraction-----")
+print("\n")
+
+starting= int(input("Enter a starting integer: "))
+add= int(input("Enter an integer to add: "))
+subtract= (int(input("Enter an integer to subtract: ")))
+
+print("\n")
+answer= starting + add - subtract
+print(starting, "+", add, "-", subtract, "is equal to", answer )
