ab52201f9ab5980fb84e3a7b161fe05c552b0316
malaveb2185 <malaveb2185@student.faytechcc.edu>
2025-04-04 11:16:02 -0400

    Add files via upload
    First submission

---
+++ PLAB1_MalaveBrandon.py
@@ -0,0 +1,7 @@
+#Brandon Malave 
+#CTI-110
+#PLAB1
+#04/04/2025
+first_name= input ("Enter your first name: ")
+last_name= input ("Enter your last name: ")
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")
